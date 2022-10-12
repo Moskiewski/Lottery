61241a7ab9d4fdd6598d879fedd92747f9aede10
Mateusz Zagrzejewski <myemail@myemail.com>
2022-10-12 15:36:44 +0100

    first commit

---
+++ src/main/java/io/lottery/NumberDisplayer.java
@@ -0,0 +1,10 @@
+package io.lottery;
+
+public class NumberDisplayer {
+    public void displayUserNumber(int[] userNumber) {
+        System.out.println("Your numbers are:");
+        for (int i : userNumber) {
+            System.out.print(i + " ");
+        }
+    }
+}
